acf2a774cb11493c9174b76ac49c1d6a623aeaac
Fabio Matos <fabio.thunder@hotmail.com>
2023-03-15 21:14:04 -0300

    initial commit

---
+++ src/main/java/com/api/MeAdote/MeAdoteApplication.java
@@ -0,0 +1,13 @@
+package com.api.MeAdote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeAdoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeAdoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/MeAdote/MeAdoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.MeAdote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeAdoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
